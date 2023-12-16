7cd24f058e3bb94a0a1f731fe7104b6ded7e3767
alwinsDen <jsmadesimple@gmail.com>
2023-12-16 23:59:03 +0530

    feat(UI): Improved visual components.

--- src/components/SelectMove/index.tsx
+++ src/components/SelectMove/index.tsx
@@ -1,17 +1,27 @@
-import React from "react"
+import React, {Dispatch, SetStateAction} from "react"
 import "./index.scss"
-const SelectMove=()=>{
+
+const SelectMove = ({setOptionState, optionState}: {
+    setOptionState: Dispatch<SetStateAction<number>>,
+    optionState: number
+}) => {
     let listNames = [
-        {name: "Rock", icon:"🪨"},
-        {name: "Paper", icon:"📄"},
-        {name: "Scissors", icon:"✂️"},
-        {name: "Spock", icon:"🖖"},
-        {name: "Lizard", icon:"🦎"},
+        {name: "Rock", icon: "🪨"},
+        {name: "Paper", icon: "📄"},
+        {name: "Scissors", icon: "✂️"},
+        {name: "Spock", icon: "🖖"},
+        {name: "Lizard", icon: "🦎"},
     ]
     return <div className={"selectmove"}>
         {
-            listNames.map((vls)=> {
-                return <button className={"indi-select"}>
+            listNames.map((vls, index) => {
+                return <button className={"indi-select"} type={"button"} onClick={() => {
+                    setOptionState(index + 1)
+                }}
+                               style={{
+                                   background: optionState === index + 1 ? "skyblue" : ""
+                               }}
+                >
                     <p className={"logo"}>{vls.icon}</p>
                     <p>{vls.name}</p>
                 </button>
--- src/pages/CreateNewGame/index.tsx
+++ src/pages/CreateNewGame/index.tsx
@@ -1,9 +1,33 @@
-import react from "react"
+import react, {useState} from "react"
 import SelectMove from "../../components/SelectMove";
 import "./index.scss"
 const CreateNewGame = () => {
-    return <div className={"createNewGame"}>
-        <SelectMove/>
-    </div>
+    //states
+    const [optionState, setOptionState] = useState(1)
+    return <form className={"createNewGame"}>
+        <div className={"createNewGame-centerComp"}>
+            <div className={"player2Key"}>
+                <p>Player 2 Public key:</p>
+                <input placeholder={"e.g. 0x2293E084Ce76BD9F3345345K"} required={true}/>
+            </div>
+            <SelectMove optionState={optionState} setOptionState={setOptionState}/>
+            <div className={"player2Key"}>
+                <p>Stake ETH Amount: </p>
+                <input placeholder={"ETH"} required={true} type={"number"}/>
+            </div>
+            <div className={"player2Key"}>
+                <p>Selected Option: </p>
+                <input style={{width:"20px"}} disabled={true} required={true} value={optionState}/>
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                <p>Secret salt: </p>
+                <input type={"password"} required={true}/>
+            </div>
+            <button className={"startGameButton"} type={"submit"}>
+                Start Game
+            </button>
+        </div>
+    </form>
 }
 export default CreateNewGame
